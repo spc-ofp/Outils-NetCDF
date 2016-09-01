62fb33924bda87a416bd7144b4a661cc52be19ed
Fabrice Bouyé <fabriceb@spc.int>
2016-09-01 11:14:10 +1100

    Added a button that allows to select all variables for a file.

--- NetCDFExtractor-j/src/org/spc/ofp/project/netcdfextractor/cell/NetCDFTreeCell.java
+++ NetCDFExtractor-j/src/org/spc/ofp/project/netcdfextractor/cell/NetCDFTreeCell.java
@@ -5,9 +5,14 @@
  ***********************************************************************/
 package org.spc.ofp.project.netcdfextractor.cell;
 
+import javafx.css.PseudoClass;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Tooltip;
 import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
+import org.spc.ofp.project.netcdfextractor.Main;
 import org.spc.ofp.project.netcdfextractor.data.FileInfo;
 import org.spc.ofp.project.netcdfextractor.data.VariableInfo;
 
@@ -23,6 +28,9 @@ import org.spc.ofp.project.netcdfextractor.data.VariableInfo;
  * @see org.spc.ofp.project.netcdfextractor.data.VariableInfo
  */
 public final class NetCDFTreeCell extends TreeCell {
+
+    private static PseudoClass FILE_PSEUDO_CLASS = PseudoClass.getPseudoClass("file"); // NOI18N.    
+    private static PseudoClass VARIABLE_PSEUDO_CLASS = PseudoClass.getPseudoClass("variable"); // NOI18N.    
 
     /**
      * Delegated checkbox.
@@ -58,8 +66,11 @@ public final class NetCDFTreeCell extends TreeCell {
         }
         String text = null;
         Node graphic = null;
+        pseudoClassStateChanged(FILE_PSEUDO_CLASS, false);
+        pseudoClassStateChanged(VARIABLE_PSEUDO_CLASS, false);
         if (!empty && item != null) {
             if (item instanceof FileInfo) {
+                pseudoClassStateChanged(FILE_PSEUDO_CLASS, true);
                 final FileInfo info = (FileInfo) item;
                 text = info.toString();
 //                graphic = checkBox;
@@ -67,7 +78,15 @@ public final class NetCDFTreeCell extends TreeCell {
 //                    object = info;
 //                    checkBox.selectedProperty().bindBidirectional(info.selectedProperty());
 //                }
+                final Tooltip buttonTip = new Tooltip();
+                buttonTip.setText(Main.I18N.getString("tree.actions.select-all-variables")); // NOI18N.
+                final Button button = new Button();
+                button.setText(Main.I18N.getString("icon.fa-check-square-o")); // NOI18N.
+                button.setOnAction(actionEvent -> selectAllVariables());
+                button.setTooltip(buttonTip);
+                graphic = button;
             } else if (item instanceof VariableInfo) {
+                pseudoClassStateChanged(VARIABLE_PSEUDO_CLASS, true);
                 final VariableInfo info = (VariableInfo) item;
                 text = info.toString();
                 graphic = checkBox;
@@ -79,5 +98,19 @@ public final class NetCDFTreeCell extends TreeCell {
         }
         setText(text);
         setGraphic(graphic);
+    }
+
+    /**
+     * Select all variables items underneath a file item.
+     */
+    private void selectAllVariables() {
+        final TreeItem<Object> fileItem = getTreeItem();
+        if (fileItem == null || !(fileItem.getValue() instanceof FileInfo)) {
+            return;
+        }
+        fileItem.getChildren()
+                .stream()
+                .map(variableItem -> (VariableInfo) variableItem.getValue())
+                .forEach(variableInfo -> variableInfo.setSelected(true));
     }
 }
