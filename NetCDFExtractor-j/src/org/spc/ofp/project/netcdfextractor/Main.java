50792943fccdeb6265deda8ed8e93ef2caab040d
Fabrice Bouyé <fabriceb@spc.int>
2016-09-15 08:07:27 +1100

    Added preliminary icon for software.

--- NetCDFExtractor-j/src/org/spc/ofp/project/netcdfextractor/Main.java
+++ NetCDFExtractor-j/src/org/spc/ofp/project/netcdfextractor/Main.java
@@ -13,10 +13,12 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.Preferences;
+import java.util.stream.IntStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -55,23 +57,31 @@ public final class Main extends Application {
         final double minHeight = 400;
         primaryStage.setMinWidth(minWidth);
         primaryStage.setMinHeight(minHeight);
-        final double stageWidth = prefs.getDouble("stage.width", 800);
-        final double stageHeight = prefs.getDouble("stage.height", 600);
-        final double stageX = prefs.getDouble("stage.x", (Screen.getPrimary().getBounds().getWidth() - stageWidth) / 2d);
-        final double stageY = prefs.getDouble("stage.y", (Screen.getPrimary().getBounds().getHeight() - stageHeight) / 2d);
+        final double stageWidth = prefs.getDouble("stage.width", 800); // NOI18N.     
+        final double stageHeight = prefs.getDouble("stage.height", 600); // NOI18N.     
+        final double stageX = prefs.getDouble("stage.x", (Screen.getPrimary().getBounds().getWidth() - stageWidth) / 2d); // NOI18N.     
+        final double stageY = prefs.getDouble("stage.y", (Screen.getPrimary().getBounds().getHeight() - stageHeight) / 2d); // NOI18N.     
         primaryStage.setX(stageX);
         primaryStage.setY(stageY);
         primaryStage.setWidth(stageWidth);
         primaryStage.setHeight(stageHeight);
+        final Image[] icons = IntStream.of(16, 24, 32, 48, 64, 128, 256, 512)
+                .mapToObj(index -> String.format("NetCDFExtractor-%d.png", index)) // NOI18N.     
+                .map(Main.class::getResource)
+                .filter(url -> url != null)
+                .map(URL::toExternalForm)
+                .map(Image::new)
+                .toArray(Image[]::new);
+        primaryStage.getIcons().setAll(icons);
         final List<Screen> screenList = Screen.getScreensForRectangle(stageX, stageY, stageWidth, stageHeight);
         if (screenList.isEmpty()) {
             primaryStage.centerOnScreen();
         }
         primaryStage.show();
-        primaryStage.xProperty().addListener(observable -> prefs.putDouble("stage.x", primaryStage.getX()));
-        primaryStage.yProperty().addListener(observable -> prefs.putDouble("stage.y", primaryStage.getY()));
-        primaryStage.widthProperty().addListener(observable -> prefs.putDouble("stage.width", primaryStage.getWidth()));
-        primaryStage.heightProperty().addListener(observable -> prefs.putDouble("stage.height", primaryStage.getHeight()));
+        primaryStage.xProperty().addListener(observable -> prefs.putDouble("stage.x", primaryStage.getX())); // NOI18N.    
+        primaryStage.yProperty().addListener(observable -> prefs.putDouble("stage.y", primaryStage.getY())); // NOI18N.    
+        primaryStage.widthProperty().addListener(observable -> prefs.putDouble("stage.width", primaryStage.getWidth())); // NOI18N.    
+        primaryStage.heightProperty().addListener(observable -> prefs.putDouble("stage.height", primaryStage.getHeight())); // NOI18N.    
 //        ScenicView.show(scene);
     }
 
